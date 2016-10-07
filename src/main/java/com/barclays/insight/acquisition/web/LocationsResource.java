2ab8e92f04366dc994be07c59af15c8d1920fd1c
Rob Brazier <rob@brzr.co>
2016-10-07 10:07:42 +0100

    update locations

--- src/main/java/com/barclays/insight/acquisition/web/LocationsResource.java
+++ src/main/java/com/barclays/insight/acquisition/web/LocationsResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 @RestController
@@ -21,7 +22,7 @@ public class LocationsResource {
         final List<Result> results = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                results.add(getResult(j, String.valueOf(i)));
+                results.add(getResult(center, j, String.valueOf(i)));
             }
         }
         final Location locations = new Location();
@@ -39,7 +40,7 @@ public class LocationsResource {
         center.setLng("-2.4557895");
         final List<Result> results = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
-            results.add(getResult(i, id));
+            results.add(getResult(center, i, id));
         }
         final Location locations = new Location();
         locations.setCenter(center);
@@ -48,16 +49,37 @@ public class LocationsResource {
         return locations;
     }
 
-    private Result getResult(final int id, final String custId) {
+    private Result getResult(final GeoLocation centre, final int id, final String custId) {
 
         final Result result = new Result();
-        final GeoLocation location = new GeoLocation();
-        location.setLat("53.47773" + (39 - id) * 12);
-        location.setLng("-2.24473" + (79 - id) * 11);
         result.setCustid(custId);
-        result.setLocation(location);
+        result.setLocation(getLocation(centre, 2000));
         result.setDetails("item=sandwich ,qty=" + id + ", totalspend=15.75 ,sex=f, age=45");
         return result;
+    }
+
+    private GeoLocation getLocation(final GeoLocation centre, int radius) {
+        Random random = new Random();
+
+        // Convert radius from meters to degrees
+        double radiusInDegrees = radius / 111000f;
+
+        double u = random.nextDouble();
+        double v = random.nextDouble();
+        double w = radiusInDegrees * Math.sqrt(u);
+        double t = 2 * Math.PI * v;
+        double x = w * Math.cos(t);
+        double y = w * Math.sin(t);
+
+        // Adjust the x-coordinate for the shrinking of the east-west distances
+        double new_x = x / Math.cos(Double.valueOf(centre.getLat()));
+
+        double foundLongitude = new_x + Double.valueOf(centre.getLng());
+        double foundLatitude = y + Double.valueOf(centre.getLat());
+        final GeoLocation location = new GeoLocation();
+        location.setLat(String.valueOf(foundLatitude));
+        location.setLng(String.valueOf(foundLongitude));
+        return location;
     }
 
 }
